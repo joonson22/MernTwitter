ab013d19d47eefda17835aadbc56eb6fff5507ec
Johnson <joonson22@gmail.com>
2019-09-22 23:46:44 -0700

    Added user auth

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const mongoose = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const passport = require('passport');
 
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
@@ -17,5 +18,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.get("/", (req, res) => res.send("Hello World!!!!"));
+app.use(passport.initialize());
+require('./config/passport')(passport)
 app.listen(port, () => console.log(`Server is running on port ${port}`));
